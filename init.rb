5c72c9f89714348271923f8e97fbf7a805e15cd1
Takashi Masuyama <mamewotoko@gmail.com>
2020-08-29 15:22:32 +0900

    increment version

--- init.rb
+++ init.rb
@@ -4,10 +4,10 @@ require_dependency 'aceeditor_preference_patch'
 require_dependency 'aceeditor_controller_patch'
 
 Redmine::Plugin.register :redmine_aceeditor do
-    name 'Redmine AceEditor plugin'
-    author 'Takashi Masuyama'
-    description 'This is a Ace editor plugin for Redmine wiki'
-    version '0.0.1'
+  name 'Redmine AceEditor plugin'
+  author 'Takashi Masuyama'
+  description 'This is a Ace editor plugin for Redmine wiki'
+  version '0.0.2'
   url 'https://github.com/mamewotoko/redmine_aceeditor'
   author_url 'mailto:mamewotoko@gmail.com'
 end
